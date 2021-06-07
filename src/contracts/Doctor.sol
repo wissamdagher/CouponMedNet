a7d70c1055277b739dc2535d8ba111af587f8165
Wissam <wissam-dagher@hotmail.com>
2021-06-07 10:05:26 +0300

    Added Doctor contract

---
+++ src/contracts/Doctor.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.5.0;
+
+contract Doctor {
+  string name;
+  address owner;
+  uint public doctorCount; 
+
+  mapping(uint => Doc) public doctors;
+  mapping(string => uint) docInvitations;
+
+  struct Doc{
+    uint id,
+    uint doctorId,
+    address owner,
+    bool active
+  }
+
+  constructor() public {
+    name = "Doctor contract initialised";
+    doctorCount = 0;
+    owner = msg.sender;
+  }
+
+  function inviteDocs(uint _docId, uint _UUID) public {
+    if(docInvitations[_UUID] > 0) {
+    emit invitationCreated("Doctor invitation", "failure");      
+    } else 
+    {
+    docInvitations[_UUID] = _docId;
+    emit invitationCreated("Doctor invitation", "success");
+    }
+  }
+
+  function registerDoc(uint _doctorId) {
+    doctorCount ++;
+    doctors[_doctorId] = Doc(doctorCount, _doctorId, msg.sender, true);
+    emit doctorRegistration("success")
+  }
+
+
+
+
+}
